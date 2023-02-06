165f9a4ff2b860eac67ed2aa1c96a0eb24deb841
priyags619 <priysgs619@gmail.com>
2023-02-06 14:06:28 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.css";
-
+//import "bootstrap/dist/css/bootstrap.css";
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; 
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
